eb9fc542f932d0b1ccb0cf1761269c72b6ef584f
ztec22 <257952406+ztec22@users.noreply.github.com>
2026-05-13 17:01:39 +0200

    fix test db hostname

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :job_applications, JobApplications.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   database: "job_applications_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
